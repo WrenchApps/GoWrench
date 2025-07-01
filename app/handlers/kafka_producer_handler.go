3c1d2d44fed0680c19ffb96db2e1eb89e35db9e7
Ivan Meireles <ivanmds2@hotmail.com>
2025-07-01 11:05:36 -0300

    add tag in kafka producer

--- app/handlers/kafka_producer_handler.go
+++ app/handlers/kafka_producer_handler.go
@@ -10,6 +10,7 @@ import (
 	"wrench/app/startup/connections"
 
 	"github.com/segmentio/kafka-go"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -35,8 +36,9 @@ func (handler *KafkaProducerHandler) Do(ctx context.Context, wrenchContext *cont
 			value := bodyContext.GetBody(settings)
 
 			var key []byte
+			var keyValue string
 			if len(settings.Kafka.MessageKey) > 0 {
-				keyValue := fmt.Sprint(contexts.GetCalculatedValue(settings.Kafka.MessageKey, wrenchContext, bodyContext, settings))
+				keyValue = fmt.Sprint(contexts.GetCalculatedValue(settings.Kafka.MessageKey, wrenchContext, bodyContext, settings))
 				key = []byte(keyValue)
 			}
 
@@ -57,12 +59,22 @@ func (handler *KafkaProducerHandler) Do(ctx context.Context, wrenchContext *cont
 				bodyContext.ContentType = "text/plain"
 				bodyContext.SetBodyAction(settings, []byte(""))
 			}
+
+			handler.setSpanAttributes(span, settings.Kafka.ConnectionId, settings.Kafka.TopicName, keyValue)
 		}
 	}
 
 	if handler.Next != nil {
 		handler.Next.Do(ctx, wrenchContext, bodyContext)
 	}
+}
+
+func (handler *KafkaProducerHandler) setSpanAttributes(span trace.Span, connectionId string, topicName string, key string) {
+	span.SetAttributes(
+		attribute.String("gowrench.connections.id", connectionId),
+		attribute.String("kafka.producer.topic_name", topicName),
+		attribute.String("kafka.producer.key", key),
+	)
 }
 
 func setError(msg string, span trace.Span, wrenchContext *contexts.WrenchContext, bodyContext *contexts.BodyContext, actionSettings *settings.ActionSettings) {
