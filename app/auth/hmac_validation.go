142ea0cdbb874484fb2b89c1b0654a0447ee7095
Kauwai Lucchesi <kauwainobre@outlook.com>
2025-08-28 14:33:29 -0300

    Fix auth-validator + fix HMAC auth

--- app/auth/hmac_validation.go
+++ app/auth/hmac_validation.go
@@ -1,26 +1,14 @@
 package auth
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"net/http"
 	"wrench/app/contexts"
 	"wrench/app/cross_funcs"
 	"wrench/app/manifest/api_settings"
 )
 
-func HMACValidate(r *http.Request, authorizationSettings *api_settings.AuthorizationSettings) bool {
+func HMACValidate(wrenchContext *contexts.WrenchContext, bodyContext *contexts.BodyContext, authorizationSettings *api_settings.AuthorizationSettings) bool {
 	var data = ""
-	bodyContext := new(contexts.BodyContext)
-	wrenchContext := new(contexts.WrenchContext)
-	wrenchContext.Request = r
-
-	body, err := readAndReplaceBody(r)
-	if err != nil {
-		return false
-	}
-	bodyContext.SetBody(body)
 
 	for _, item := range authorizationSettings.ConcatenateFields {
 		data += fmt.Sprint(contexts.GetCalculatedValue(item, wrenchContext, bodyContext, nil))
@@ -32,20 +20,4 @@ func HMACValidate(r *http.Request, authorizationSettings *api_settings.Authoriza
 	actualHash := cross_funcs.GetHash(authorizationSettings.Key, hashFn, []byte(data))
 
 	return expectedHash == actualHash
-}
-
-func readAndReplaceBody(r *http.Request) ([]byte, error) {
-	if r.Body == nil {
-		return nil, nil
-	}
-
-	bodyBytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	r.Body.Close()
-	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-
-	return bodyBytes, nil
 }
